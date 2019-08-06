494319d3bf810c035d2f67580d2859cf2406a5d8
Linux <398863434@qq.com>
2019-08-06 10:02:14 +0800

    wangzy

--- dev_test/main.c
+++ dev_test/main.c
@@ -15,16 +15,16 @@ int  main(void)
 
     mHD_Software_Init();
 
-//     mHD_Send_Msg_Cmd(Msg_Link,luozx);
-//     sleep(1);
-//     mHD_Read_Shm_ShareMemory_DevData(Run_data.Shmkey,Run_data.Semkey,&Dev_data);  //读取共享内存区
-//     mHD_Data_DevData_Set();   //写入数据值
-//     mHD_Write_Shm_ShareMemory_DevData(Run_data.Shmkey,Run_data.Semkey,&Dev_data);  //写入共享内存区
-//     mHD_Send_Msg_Cmd(Msg_WritePara,luozx);
-//     sleep(1);
-//     mHD_Send_Msg_Cmd(Msg_ReadPara,luozx);
-//     sleep(1);
-//     mHD_Send_Msg_Cmd(Msg_StartScan,luozx);
+     mHD_Send_Msg_Cmd(Msg_Link,luozx);
+     sleep(1);
+     mHD_Read_Shm_ShareMemory_DevData(Run_data.Shmkey,Run_data.Semkey,&Dev_data);  //读取共享内存区
+     mHD_Data_DevData_Set();   //写入数据值
+     mHD_Write_Shm_ShareMemory_DevData(Run_data.Shmkey,Run_data.Semkey,&Dev_data);  //写入共享内存区
+     mHD_Send_Msg_Cmd(Msg_WritePara,luozx);
+     sleep(1);
+     mHD_Send_Msg_Cmd(Msg_ReadPara,luozx);
+     sleep(1);
+     mHD_Send_Msg_Cmd(Msg_StartScan,luozx);
 
     while(1)
     {
